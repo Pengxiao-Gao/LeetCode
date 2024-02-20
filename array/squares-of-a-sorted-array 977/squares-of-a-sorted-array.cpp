36c78d8e2b64715ed71528452663bb6aaa072bc0
Pengxiao-Gao <504207850@qq.com>
2024-02-20 10:46:18 +0100

    209 minimum-size-subarray-sum

---
+++ array/squares-of-a-sorted-array 977/squares-of-a-sorted-array.cpp
@@ -0,0 +1,22 @@
+// use double printers to scan the array from both ends to the middle
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        int k = A.size() - 1;
+        vector<int> result(A.size(), 0);
+        for (int i = 0, j = A.size() - 1; i <= j;) {
+            if (A[i] * A[i] < A[j] * A[j])  {
+                result[k--] = A[j] * A[j];
+                j--;
+            }
+            else {
+                result[k--] = A[i] * A[i];
+                i++;
+            }
+        }
+        return result;
+    }
+};
